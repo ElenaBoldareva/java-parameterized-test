0cd3baf4c8fc6417279cb78278d9785f3bb0eec7
Elena Boldareva <elenabold23@gmail.com>
2022-10-28 15:14:02 +0300

    My parameterized test

--- src/test/java/pageobjects/MainPage.java
+++ src/test/java/pageobjects/MainPage.java
@@ -2,85 +2,115 @@ package pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.Assert.assertEquals;
+import java.time.Duration;
 
-public class ImportantQuestionsPage {
-
+public class MainPage {
     private final WebDriver driver;
-
+    private final By cookieButton = By.id("rcc-confirm-button");
     private final By howToPayButton = By.id("accordion__heading-0");          //локаторы для выпадающего списка
-    private final By howToPayPanel = By.xpath(".//div[@id='accordion__panel-0']/p");          //локаторы для выпадающего списка
-    private final By severalScooters = By.id("accordion__heading-1");
-    private final By rentalTime = By.id("accordion__heading-2");
-    private final By orderScooterToday = By.id("accordion__heading-3");
-    private final By extendOrderOrReturnScooter = By.id("accordion__heading-4");
-    private final By chargeWithScooter = By.id("accordion__heading-5");
-    private final By canCancelOrder = By.id("accordion__heading-6");
-    private final By bringOutsideMKAD = By.id("accordion__heading-7");
+    private final By howToPayPanel = By.xpath(".//div[@id='accordion__panel-0']/p");
+    private final By severalScootersButton = By.id("accordion__heading-1");
+    private final By severalScootersPanel = By.xpath(".//div[@id='accordion__panel-1']/p");
+    private final By rentalTimeButton = By.id("accordion__heading-2");
+    private final By rentalTimePanel = By.xpath(".//div[@id='accordion__panel-2']/p");
+    private final By orderScooterTodayButton = By.id("accordion__heading-3");
+    private final By orderScooterPanel = By.xpath(".//div[@id='accordion__panel-3']/p");
+    private final By extendOrderOrReturnScooterButton = By.id("accordion__heading-4");
+    private final By extendOrderOrReturnScooterPanel = By.xpath(".//div[@id='accordion__panel-4']/p");
+    private final By chargeWithScooterButton = By.id("accordion__heading-5");
+    private final By chargeWithScooterPanel = By.xpath(".//div[@id='accordion__panel-5']/p");
+    private final By canCancelOrderButton = By.id("accordion__heading-6");
+    private final By canCancelOrderPanel = By.xpath(".//div[@id='accordion__panel-6']/p");
+    private final By bringOutsideMKADButton = By.id("accordion__heading-7");
+    private final By bringOutsideMKADPanel = By.xpath(".//div[@id='accordion__panel-7']/p");
+    private final By upButtonOrderButton = By.xpath(".//div[@class='Header_Nav__AGCXC']/button[@class='Button_Button__ra12g']");
+    private final By downButtonOrderButton = By.xpath(".//div[@class='Home_FinishButton__1_cWm']/button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
 
-    public ImportantQuestionsPage(WebDriver driver) {
+    public MainPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void clickHowToPayButton() {
-        driver.findElement(howToPayButton).click();
+        driver.findElement(howToPayButton).click();     //1
+    }
+
+    public void clickCookieButton() {
+        driver.findElement(cookieButton).click();     //1
     }
 
     public String getTextHowToPayPanel() {
-        return driver.findElement(howToPayPanel).getText();
+        return driver.findElement(howToPayPanel).getText();         //1.1
     }
 
-    // метод проверяет текст 1 кнопки
-    public void checkTextHowToPay() {
-        assertEquals("Ошибка!", "Сутки — 400 рублей. Оплата курьеру — наличными или картой.", driver.findElement(howToPayButton).getText());
-    }
+    public void clickSeveralScootersButton() {
+        driver.findElement(severalScootersButton).click();
+    }     //2
 
-    public void clickSeveralScootersButton() { driver.findElement(severalScooters); }
-    public void checkTextSeveralScootersButton() {
-        assertEquals("Ошибка!", "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.", driver.findElement(severalScooters).getText());
+    public String getTextSeveralScootersPanel(){
+        return driver.findElement(severalScootersPanel).getText();          //2.1
     }
 
     public void clickRentalTimeButton() {
-        driver.findElement(rentalTime);
-    }
-    public void checkTextRentalTimeButton() {
-        assertEquals("Ошибка!", "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.", driver.findElement(rentalTime).getText());
+        driver.findElement(rentalTimeButton).click();
+    }       //3
+
+    public String getTextRentalTimePanel(){
+        return driver.findElement(rentalTimePanel).getText();          //3.1
     }
 
     public void clickOrderScooterTodayButton() {
-        driver.findElement(orderScooterToday);
-    }
-    public void checkOrderScooterTodayButton() {
-        assertEquals("Ошибка!", "Только начиная с завтрашнего дня. Но скоро станем расторопнее.", driver.findElement(orderScooterToday).getText());
+        driver.findElement(orderScooterTodayButton).click();
+    }       //4
+
+    public String getTextOrderScooterTodayPanel(){
+        return driver.findElement(orderScooterPanel).getText();          //4.1
     }
 
-    public void clickExtendOrderOrReturnScooterButton() {
-        driver.findElement(extendOrderOrReturnScooter);
+    public void clickExtendOrderOrReturnScooterButton() {               //5
+        driver.findElement(extendOrderOrReturnScooterButton).click();
     }
-    public void checkExtendOrderOrReturnScooterButton() {
-        assertEquals("Ошибка!", "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.", driver.findElement(extendOrderOrReturnScooter).getText());
+    public String getTextExtendOrderOrReturnScooterPanel(){         //5.1
+        return driver.findElement(extendOrderOrReturnScooterPanel).getText();
     }
 
     public void clickChargeWithScooterButton() {
-        driver.findElement(chargeWithScooter);
-    }
-    public void checkChargeWithScooterButton() {
-        assertEquals("Ошибка!", "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.", driver.findElement(chargeWithScooter).getText());
+        driver.findElement(chargeWithScooterButton).click();
+    }       //6
+
+    public String getTextChargeWithScooterPanel(){                                           //6.1
+        return driver.findElement(chargeWithScooterPanel).getText();
     }
 
     public void clickCanCancelOrderButton() {
-        driver.findElement(canCancelOrder);
-    }
-    public void checkCanCancelOrderButton() {
-        assertEquals("Ошибка!", "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.", driver.findElement(canCancelOrder).getText());
+        driver.findElement(canCancelOrderButton).click();
+    }       //7
+
+    public String getTextCanCancelOrderPanel(){                                           //7.1
+        return driver.findElement(canCancelOrderPanel).getText();
     }
 
     public void clickBringOutsideMKADButton() {
-        driver.findElement(bringOutsideMKAD);
-    }
-    public void checkBringOutsideMKADButton() {
-        assertEquals("Ошибка!", "Да, обязательно. Всем самокатов! И Москве, и Московской области.", driver.findElement(bringOutsideMKAD).getText());
+        driver.findElement(bringOutsideMKADButton).click();
+    }       //8
+
+    public String getTextBringOutsideMKADPanel(){                                           //7.1
+        return driver.findElement(bringOutsideMKADPanel).getText();
     }
 
+    public void waitForLoad(int seconds){
+        new WebDriverWait(driver, Duration.ofSeconds(seconds))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.className("Home_FAQ__3uVm4")));
+    }
+
+    public void clickUpOrderButton() {
+        driver.findElement(upButtonOrderButton).click();
+    }
+
+    public void clickDownButtonOrderButton() {
+        driver.findElement(downButtonOrderButton).click();
+    }
 }
+
--- src/test/java/tests/TestMainPage.java
+++ src/test/java/tests/TestMainPage.java
@@ -1,2 +1,127 @@
-package tests;public class TestMainPage {
+package tests;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import pageobjects.MainPage;
+
+import java.time.Duration;
+
+
+public class TestMainPage {
+    private WebDriver driver;
+    private final int defaultTimeOut = 10;
+    private final String howToPayExpectedText = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+    private final String severalScootersExpectedText = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
+    private final String rentalTimeExpectedText = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
+    private final String orderScooterTodayExpectedText = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
+    private final String extendOrderOrReturnScooterExpectedText = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+    private final String chargeWithScooterExpectedText = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
+    private final String canCancelOrderExpectedText = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+    private final String bringOutsideMkadExpectedText = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
+
+    @Before
+    public void startUp() {
+        String br = System.getenv("browser");
+        if ("chrome".equals(br)) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--no-sandbox", "--headless", "--disable-dev-shm-usage");
+            driver = new ChromeDriver(chromeOptions);
+        } else if ("ff".equals(br)) {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.setHeadless(true);
+            driver = new FirefoxDriver();
+        } else {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--no-sandbox", "--headless", "--disable-dev-shm-usage");
+            driver = new ChromeDriver(chromeOptions);
+        }
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(defaultTimeOut));
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void checkHowToPay() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.waitForLoad(defaultTimeOut);
+        mainPage.clickCookieButton();
+        mainPage.waitForLoad(defaultTimeOut);
+        mainPage.clickHowToPayButton();
+        String actualText = mainPage.getTextHowToPayPanel();
+        Assert.assertEquals(howToPayExpectedText, actualText);
+    }
+
+    @Test
+    public void checkSeveralScooters() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickCookieButton();
+        mainPage.clickSeveralScootersButton();
+        String actualText = mainPage.getTextSeveralScootersPanel();
+        Assert.assertEquals(severalScootersExpectedText, actualText);
+    }
+
+    @Test
+    public void checkRentalTime() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickCookieButton();
+        mainPage.clickRentalTimeButton();
+        String actualText = mainPage.getTextRentalTimePanel();
+        Assert.assertEquals(rentalTimeExpectedText, actualText);
+    }
+
+    @Test
+    public void checkOrderScooterToday() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickCookieButton();
+        mainPage.clickOrderScooterTodayButton();
+        String actualText = mainPage.getTextOrderScooterTodayPanel();
+        Assert.assertEquals((orderScooterTodayExpectedText), actualText);
+    }
+
+    @Test
+    public void checkExtendOrderOrReturnScooter() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickCookieButton();
+        mainPage.clickExtendOrderOrReturnScooterButton();
+        String actualText = mainPage.getTextExtendOrderOrReturnScooterPanel();
+        Assert.assertEquals((extendOrderOrReturnScooterExpectedText), actualText);
+    }
+
+    @Test
+    public void checkChargeWithScooter() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickCookieButton();
+        mainPage.clickChargeWithScooterButton();
+        String actualText = mainPage.getTextChargeWithScooterPanel();
+        Assert.assertEquals((chargeWithScooterExpectedText), actualText);
+    }
+
+    @Test
+    public void checkCanCancelOrder() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickCookieButton();
+        mainPage.clickCanCancelOrderButton();
+        String actualText = mainPage.getTextCanCancelOrderPanel();
+        Assert.assertEquals((canCancelOrderExpectedText), actualText);
+    }
+
+    @Test
+    public void checkBringOutsideMKAD() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickCookieButton();
+        mainPage.clickBringOutsideMKADButton();
+        String actualText = mainPage.getTextBringOutsideMKADPanel();
+        Assert.assertEquals((bringOutsideMkadExpectedText), actualText);
+    }
 }
